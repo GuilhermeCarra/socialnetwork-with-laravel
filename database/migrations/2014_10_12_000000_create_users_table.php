932f5b934a5304d9e2023c1069dcc5094afdedbb
Ezequiel <ezemgaray@gmail.com>
2020-09-21 16:24:21 +0200

    Updat migration create_users_table - add avatar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('avatar');
             $table->text('description');
             $table->timestamps();
         });
