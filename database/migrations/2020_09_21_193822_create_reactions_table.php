2c3f2cbb6dd4a9ff82dee9a64e018a370962777d
Ezequiel <ezemgaray@gmail.com>
2020-09-21 22:37:44 +0200

    Add Migration create_reactions_table.php

---
+++ database/migrations/2020_09_21_193822_create_reactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->primary(['user_id', 'post_id']);
+            $table->boolean('is_like')->default(false);
+            $table->boolean('is_dislike')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions');
+    }
+}
