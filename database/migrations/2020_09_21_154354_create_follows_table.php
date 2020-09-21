c0bae11bba3cb02dd6e12aac1fe88f9c267e737d
Ezequiel <ezemgaray@gmail.com>
2020-09-21 18:15:12 +0200

    Add Migrations create_follows_table

---
+++ database/migrations/2020_09_21_154354_create_follows_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->foreign('user_1')->references('id')->on('users');
+            $table->foreign('user_2')->references('id')->on('users');
+            $table->int('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+}
