83615e22e222d6eae8a80e4b3690a2075b4a4f48
Ezequiel <ezemgaray@gmail.com>
2020-09-22 13:49:16 +0200

    Update routes/web.php - change rout '/' to home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
