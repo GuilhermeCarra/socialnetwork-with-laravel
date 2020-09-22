7b616f4d521c7f16e8ee19301f0b81430525aef2
Ezequiel <ezemgaray@gmail.com>
2020-09-22 16:17:02 +0200

    Update migration create_user_table - change avatar and descripion ->nullable()

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('avatar');
-            $table->text('description');
+            $table->string('avatar')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
