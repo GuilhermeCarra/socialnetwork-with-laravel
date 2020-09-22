367aa5bed476ba75c9cf9a6c9b9db377d8a7e8b5
guilherme-carra <gui_gc@hotmail.com>
2020-09-22 10:25:23 +0200

    added primaryKey variable to Follow Model

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follow extends Model
 {
+    protected $primaryKey = ['user_1','post_2'];
     protected $fillable = [
         'user_1', 'user_2'
     ];
