ce55a2cfdcfff9899c9bd33664307d85df69053f
guilherme-carra <gui_gc@hotmail.com>
2020-09-22 13:49:36 +0200

    fixed follow model to work with 2 Primary Keys

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Follow extends Model
 {
+    use HasFactory;
+
     protected $primaryKey = ['user_1','post_2'];
+    public $incrementing = false;
+
     protected $fillable = [
         'user_1', 'user_2'
     ];
