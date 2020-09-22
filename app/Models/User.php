c2c96b1c2fd98019362f15e9a24127d114d4d1d9
Ezequiel <ezemgaray@gmail.com>
2020-09-22 16:33:28 +0200

    Update Model User.php - Adding 'use SoftDeletes' and 'protected '

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,22 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+
+    protected $dates = ['deleted_at'];
+
     protected $fillable = [
         'username', 'name', 'email', 'password', 'description', 'avatar'
     ];
