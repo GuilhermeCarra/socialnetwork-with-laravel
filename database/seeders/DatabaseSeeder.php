8613c8efd9cb6d7e3d931f625b845af322dfc1f4
guilherme-carra <gui_gc@hotmail.com>
2020-09-23 14:45:47 +0200

    added factories and terminal messages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Follow;
 use App\Models\Post;
 use App\Models\Reaction;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,10 +21,22 @@ class DatabaseSeeder extends Seeder
         if(!isset($GLOBALS['followsArray'])) $GLOBALS['followsArray']= array();
         if(!isset($GLOBALS['reactionsArray'])) $GLOBALS['reactionsArray']= array();
 
-        //User::factory(200)->create();
-        //Follow::factory(2500)->create();
-        //Post::factory(1000)->create();
-        //Reaction::factory(1000)->create();
-        //Post::factory()->fixReactionsCount();
+        print 'Generating users...' . PHP_EOL;
+        User::factory(200)->create();
+
+        print 'Generating follows...' . PHP_EOL;
+        Follow::factory(2500)->create();
+
+        print 'Generating posts...' .PHP_EOL ;
+        Post::factory(1000)->create();
+
+        print 'Generating posts reactions...' . PHP_EOL;
+        Reaction::factory(2500)->create();
+
+        print 'Generating posts comments...' . PHP_EOL;
+        Comment::factory(2500)->create();
+
+        print 'Fixing likes, dislikes and comments counts...' . PHP_EOL;
+        Post::factory()->fixReactionsCount();
     }
 }
