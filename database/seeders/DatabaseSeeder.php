f07588b74726841a1009feb58b1410c87da9752f
guilherme-carra <gui_gc@hotmail.com>
2020-09-23 13:24:15 +0200

    added factories to DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Follow;
+use App\Models\Post;
+use App\Models\Reaction;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         if(!isset($GLOBALS['followsArray'])) $GLOBALS['followsArray']= array();
+        if(!isset($GLOBALS['reactionsArray'])) $GLOBALS['reactionsArray']= array();
 
-        User::factory(200)->create();
-        Follow::factory(2500)->create();
+        //User::factory(200)->create();
+        //Follow::factory(2500)->create();
+        //Post::factory(1000)->create();
+        //Reaction::factory(1000)->create();
+        //Post::factory()->fixReactionsCount();
     }
 }
