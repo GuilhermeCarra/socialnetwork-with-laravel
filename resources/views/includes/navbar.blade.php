a408bd1d9fbeececd03201e90abefe13f1160062
Ezequiel <ezemgaray@gmail.com>
2020-09-24 22:54:48 +0200

    Update View navbar - include search button

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -26,7 +26,7 @@
             @endif
             @else
             <li class="nav-item">
-                @include('includes.search')
+                @include('includes.search_button')
             </li>
             <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
