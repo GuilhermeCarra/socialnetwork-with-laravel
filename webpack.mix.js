1844856d0c9fb19a0fd0dcedbe837fd9f9a80bd7
Ezequiel <ezemgaray@gmail.com>
2020-09-24 03:32:44 +0200

    Update webpack.mix.js - Update Sass file extension

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/assets/js')
-    .sass('resources/sass/app.scss', 'public/assets/css');
+    .sass('resources/sass/app.sass', 'public/assets/css');
