c16a27eda15ab96d3a0e0ad37abc35289cc2cb5e
Ezequiel <ezemgaray@gmail.com>
2020-09-24 02:59:20 +0200

    Update Profile View - Change condition to show button follow/unfollow

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,13 +11,13 @@
                     <p><b>{{$user->name}}</b></p>
                     <p>{{$user->description}}</p>
                     @if($user->id != auth()->user()->id)
-                        <form method="POST" action="{{$following ? route('follow') : route('unfollow')}}">
+                        <form method="POST" action="{{$following ? route('unfollow') : route('follow')}}">
                             @if($user->id != auth()->user()->id && $following) 
                                 @method('DELETE')
                             @endif
                             @csrf
                             <input type="hidden" name="follower" value="{{auth()->user()->id}}">
-                            <input type="hidden" name="follwed" value="{{$user->id}}">
+                            <input type="hidden" name="followed" value="{{$user->id}}">
                             <button type="submit">{{$following ? 'Unfollow' : 'Follow'}}</button>
                         </form>
                     @endif
