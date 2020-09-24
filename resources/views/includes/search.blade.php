ca81a8311a2a8a9afb713b22e9d86c1c81199824
Ezequiel <ezemgaray@gmail.com>
2020-09-24 13:01:22 +0200

    Add Views includes/search.blade - Search html structure

---
+++ resources/views/includes/search.blade.php
@@ -0,0 +1,7 @@
+<div class="search__container">
+    <input type="checkbox" class="checkbox" id="check-search">
+    <label for="check-search" class="search-label bg-red-primary">
+        <i class="ri-search-line text-white"></i>
+    </label>
+    <input type="seach" class="search-input" id="search-input">
+</div>
