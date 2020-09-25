255600bcb9a75529ca46bae37adc44926c676926
Ezequiel <ezemgaray@gmail.com>
2020-09-25 05:10:04 +0200

    Update View search_button - Adding id #search-button

--- resources/views/includes/search_button.blade.php
+++ resources/views/includes/search_button.blade.php
@@ -1,4 +1,4 @@
-<div class="search__button">
+<div class="search__button" id="search-button">
     <div class="search-label bg-blue-primary">
         <i class="ri-search-line text-white"></i>
     </div>
