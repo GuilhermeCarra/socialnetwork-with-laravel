d76d6f0390d912dab08586cf58db6af86ce9ffd3
guilherme-carra <gui_gc@hotmail.com>
2020-09-25 10:22:22 +0200

    fixed wrong variable name posts to postsPage

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import 'remixicon/fonts/remixicon.css'
 
 var postsPage = 1;
 $(window).on('scroll', function(){
-    if(Math.ceil($(window).scrollTop()) + Math.ceil($(window).height()) >= $(document).height()) {
+    if(Math.ceil($(window).scrollTop()) + Math.ceil($(window).height()) >= $(document).height()  && postsPage) {
             postsPage++;
             loadPosts(postsPage);
         }
@@ -11,13 +11,12 @@ $(window).on('scroll', function(){
 
 function loadPosts(page) {
     $.ajax({
-        url: 'home?page=' + page,
+        url: 'home?page=' + postsPage,
         type: 'GET',
         beforeSend: function() {
             $('#load-message').removeClass('d-none');
         }
     }).done(function(data){
-        console.log(data.html)
         if(data.html.length == 0){
             postsPage = false;
             $('#load-message').text('No more posts to show').removeClass('d-none');
