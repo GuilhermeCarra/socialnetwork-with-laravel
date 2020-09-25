c6dfbec2a66313351b4c3c4b1cfffa13c9284f32
Ezequiel <ezemgaray@gmail.com>
2020-09-25 05:11:26 +0200

    Update View search.blade - Adding id 'search' and 'search-form'

--- resources/views/includes/search.blade.php
+++ resources/views/includes/search.blade.php
@@ -1,5 +1,5 @@
-<div class="search d-flex flex-column justify-content-center align-items-center">
-    <div class="search-form">
+<div id="search" class="search d-flex flex-column justify-content-center align-items-center">
+    <div class="search-form" id="search-form">
         <div class="form-group search-input-container">
             <input type="search" name="search" id="search-input" class="search-input form-control text-white shadow" autocomplete="off">
             <div class="search-close text-white">
