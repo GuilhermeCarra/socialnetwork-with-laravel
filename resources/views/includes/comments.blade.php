6dca706a4bad4f07b6a6641da61093cc02b10837
Ezequiel <ezemgaray@gmail.com>
2020-09-26 05:27:11 +0200

    Update Views - Update comments structure (full view)

--- resources/views/includes/comments.blade.php
+++ resources/views/includes/comments.blade.php
@@ -1,4 +1,28 @@
 @foreach ($comments as $comment)
+    <div class="comment preview my-2">
+        <div class="d-flex flex-start flex-nowrap align-items-center">
+            <div class="comment__avatar__box">
+                <a class="" href="{{ $commentingUsers[$comment->user_id]->username }}">
+                    <img src="{{ $commentingUsers[$comment->user_id]->avatar }}" class="rounded-circle avatar" alt="avatar">
+                </a>
+            </div>
+            <div class="comment__content d-flex flex-column">
+                <div class="d-flex flex-wrap m-0">
+                    <a class="mr-2" href="{{ $commentingUsers[$comment->user_id]->username }}">
+                        {{$commentingUsers[$comment->user_id]->name }}
+                    </a>
+                    <p class="p-0 m-0"><small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small></p>
+                </div>
+                <div class="line-clamp">
+                    <p class="card-text">{{ $comment->content }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach
+
+
+{{-- @foreach ($comments as $comment)
     <div class="comment">
         <div class="container-fluid d-flex">
             <img src="{{ $users[$comment->user_id]->avatar }}" class="col-1 rounded-circle" alt="avatar">
@@ -7,4 +31,4 @@
         <p class="card-text">{{ $comment->content }}</p>
         <p class="card-text"><small class="text-muted">{{ $comment->created_at }}</small></p>
     </div>
-@endforeach
+@endforeach --}}
