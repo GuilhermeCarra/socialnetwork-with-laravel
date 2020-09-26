3678bd8c97377fafd0c777a15a810dc23aaa51d0
guilherme-carra <gui_gc@hotmail.com>
2020-09-26 10:37:33 +0200

    fixed variable names

--- resources/views/includes/comments.blade.php
+++ resources/views/includes/comments.blade.php
@@ -2,14 +2,14 @@
     <div class="comment preview my-2">
         <div class="d-flex flex-start flex-nowrap align-items-center">
             <div class="comment__avatar__box">
-                <a class="" href="{{ $commentingUsers[$comment->user_id]->username }}">
-                    <img src="{{ $commentingUsers[$comment->user_id]->avatar }}" class="rounded-circle avatar" alt="avatar">
+                <a class="" href="{{ $users[$comment->user_id]->username }}">
+                    <img src="{{ $users[$comment->user_id]->avatar }}" class="rounded-circle avatar" alt="avatar">
                 </a>
             </div>
             <div class="comment__content d-flex flex-column">
                 <div class="d-flex flex-wrap m-0">
-                    <a class="mr-2" href="{{ $commentingUsers[$comment->user_id]->username }}">
-                        {{$commentingUsers[$comment->user_id]->name }}
+                    <a class="mr-2" href="{{ $users[$comment->user_id]->username }}">
+                        {{ $users[$comment->user_id]->name }}
                     </a>
                     <p class="p-0 m-0"><small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small></p>
                 </div>
