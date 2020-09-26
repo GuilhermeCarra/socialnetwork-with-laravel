17e8b91e625f9b96cc61241d1073d87e7dd82610
Ezequiel <ezemgaray@gmail.com>
2020-09-26 18:12:14 +0200

    Update JS search.js - search modal correction Fix for chrome compatibility

--- resources/js/customjs/search.js
+++ resources/js/customjs/search.js
@@ -12,13 +12,12 @@
         },
         bindEvents: function(){
             this.button.on('click', this.showSearch.bind(this))
-            this.input.on('input', this.showResults)
+            this.input.on('input', this.showResults.bind(this))
             this.form.on('click', '.search-close', this.closeSearch.bind(this))
-
         },
         showSearch: function(){
             $('body').addClass('stop-scroll')
-            this.search.addClass('show').css('top', window.top.scrollY)
+            this.search.addClass('show')
             setTimeout(() => {this.input.focus()}, 500);
         },
         showResults: function(){
@@ -29,7 +28,6 @@
             $('#search-result').removeClass('searching')
             $('body').removeClass('stop-scroll')
             this.search.removeClass('show')
-
         }
     }
     search.init()
