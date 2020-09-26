fcf1f9c817aa0c42049b765a8c52765c8bd8f017
Ezequiel <ezemgaray@gmail.com>
2020-09-26 05:19:50 +0200

    Update webpack.mix.js - change popper url to public/assets/js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/assets/js')
-    .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
+    .js('node_modules/popper.js/dist/popper.js', 'public/assets/js').sourceMaps()
     .sass('resources/sass/app.sass', 'public/assets/css');
