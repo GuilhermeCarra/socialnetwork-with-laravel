4f63031b5bf513620d0577a1e0cd2a7e87aac270
Ezequiel <ezemgaray@gmail.com>
2020-09-26 05:26:02 +0200

    Update Views ppst.blade - Check isset(), set  value, fedine post structure with comments preview

--- resources/views/includes/post.blade.php
+++ resources/views/includes/post.blade.php
@@ -1,12 +1,19 @@
-<div class="card post">
-    <div class="card-header">
-        <div class="post__header d-flex flex-nowrap justify-content-between align-items-center">
-            <div class="post__header__user">
-                <a class=" font-weight-bold" href="{{ $friends[$post->user_id]->username }}">
-                    <img src="{{ $friends[$post->user_id]->avatar }}" class="rounded-circle avatar" alt="avatar">
-                    {{ $friends[$post->user_id]->name }}
+
+@foreach ($posts as $post)
+@if(isset($friends))
+    @php 
+        $user = $friends[$post->user_id] 
+    @endphp
+@endif
+    <div class="card post">
+        <div class="card-header">
+            <div class="post__header d-flex flex-nowrap justify-content-between align-items-center">
+                <div class="post__header__user">
+                <a class=" font-weight-bold" href="{{$user->username}}">
+                    <img src="{{ $user->avatar }}" class="rounded-circle avatar" alt="avatar">
+                    {{ $user->name }}
                 </a>
-                <small> {{'@'.$friends[$post->user_id]->username }}</small>
+                <small> {{'@'.$user->username }}</small>
             </div>
             <div class="post__header__menu">
                 <div class="post__header__menu--btn">
@@ -18,7 +25,7 @@
     <div class="card-body">
         <div class="post__body">
             <div class="post__body__content">
-                <p class="created"><small class="text-muted">{{ $post->created_at->diffForHumans() }}</small></p>
+                <p class="p-0 m-0"><small class="text-muted">{{ $post->created_at->diffForHumans() }}</small></p>
                 <p class="card-text line-clamp-3">{{ $post->description }}</p>
             </div>
             @if($post->image)
@@ -44,29 +51,56 @@
     </div>
     <div class="card-footer">
         <!-- Comments -->
+        <div class="comment preview my-2">
+            <div class="d-flex flex-start flex-nowrap align-items-center">
+                <div class="comment__avatar__box">
+                    <a class="" href="{{ auth()->user()->username }}">
+                        <img src="{{ auth()->user()->avatar }}" class="rounded-circle avatar" alt="avatar">
+                    </a>
+                </div>
+                <div class="comment__create d-flex flex-column w-100">
+                    <div class="d-flex flex-wrap m-0">
+                        <a class="mr-2" href="{{ auth()->user()->username }}">
+                            {{auth()->user()->name}}
+                        </a>
+                        <p class="p-0 m-0"><small class="text-muted">Add a comment!</small></p>
+                    </div>
+                    <div class="comment_create__add">
+                        <div class="form-group d-flex flex-nowrap justify-content-between">
+                            <textarea class="form-control" name="newcomment" id="coment" cols="30" rows="1"></textarea>
+                            <button class="btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         @foreach ($comments as $comment)
             @if($post->id == $comment->post_id)
-                <div class="container-fluid d-flex comment preview my-2">
+                <div class="comment preview my-2">
                     <div class="d-flex flex-start flex-nowrap align-items-center">
                         <div class="comment__avatar__box">
                             <a class="" href="{{ $commentingUsers[$comment->user_id]->username }}">
-                                <img width="30px" height="30px" src="{{ $commentingUsers[$comment->user_id]->avatar }}" class="rounded-circle avatar" alt="avatar">
+                                <img src="{{ $commentingUsers[$comment->user_id]->avatar }}" class="rounded-circle avatar" alt="avatar">
                             </a>
                         </div>
                         <div class="comment__content d-flex flex-column">
-                            <div class="d-flex flex-nowrap m-0">
+                            <div class="d-flex flex-wrap m-0">
                                 <a class="mr-2" href="{{ $commentingUsers[$comment->user_id]->username }}">
                                     {{$commentingUsers[$comment->user_id]->name }}
                                 </a>
-                                <p class="created"><small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small></p>
+                                <p class="p-0 m-0"><small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small></p>
                             </div>
-                            <div class="line-clamp-2">
+                            <div class="line-clamp">
                                 <p class="card-text">{{ $comment->content }}</p>
                             </div>
                         </div>
+                    </div>
+                    <div class="mt-4 mb-2 p-0 btn text-center w-100 bg-disabled pointer" id="post_{{$post->id}}">
+                        See more comments...
                     </div>
                 </div>
             @endif
         @endforeach
     </div>
 </div>
+@endforeach
