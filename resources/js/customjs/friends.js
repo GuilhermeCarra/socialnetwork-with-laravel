db6902ee4e8c57e8b60b19b999c0bf33740086bc
Ezequiel <ezemgaray@gmail.com>
2020-09-27 23:38:52 +0200

    Update JS friends.js - remove this.showFriends()

--- resources/js/customjs/friends.js
+++ resources/js/customjs/friends.js
@@ -8,18 +8,11 @@
             this.friends = $('#friends')
             this.form = $('#friends-form')
             this.input = $('#friends-input')
-            this.button = $('#menu-friends-btn')
             this.results = $('#friends-result')
         },
         bindEvents: function(){
-            this.button.on('click', this.showFriends.bind(this))
             this.input.on('input', this.showResults.bind(this))
             this.form.on('click', '.friends-close', this.closeFriends.bind(this))
-        },
-        showFriends: function(){
-            $('body').addClass('stop-scroll')
-            this.friends.addClass('show')
-            setTimeout(() => {this.input.focus()}, 500);
         },
         showResults: function(){
             this.results.addClass('searching').fadeIn()
