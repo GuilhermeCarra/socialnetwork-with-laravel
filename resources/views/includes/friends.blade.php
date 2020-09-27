13574f805a1f77f100fc8fcbdfe29c256cdf1dc4
Ezequiel <ezemgaray@gmail.com>
2020-09-27 18:20:13 +0200

    Add Views includes/friends.blade - Display friend list

---
+++ resources/views/includes/friends.blade.php
@@ -0,0 +1,11 @@
+<div id="friends" class="friends d-flex flex-column justify-content-center align-items-center">
+    <div class="friends-form" id="friends-form">
+        <div class="form-group friends-input-container">
+            <input type="search" name="friends" id="friends-input" class="friends-input form-control text-white shadow" autocomplete="off">
+            <div class="friends-close text-white">
+                <i class="ri-close-circle-line text-white"></i>
+            </div>
+        </div>
+    </div>
+    <div class="friends-list" id="friends-result"></div>
+</div>
