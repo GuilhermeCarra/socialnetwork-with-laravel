b3014794318950cbd57d391526cf01fbc01c24e4
Ezequiel <ezemgaray@gmail.com>
2020-09-27 08:19:17 +0200

    Update Post Model - Adding getPostByUserId() method and documentation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,15 +13,34 @@ class Post extends Model
         'description', 'image', 'likes_count', 'dislikes_count', 'comments_count'
     ];
 
-    protected static function getPostByUserId($id, $paginate = 5){
-        return self::where('user_id', $id)->orderBy('created_at','desc')->paginate($paginate);
+    /**
+     * Get posts by User id
+     * Pagination by default: 5
+     * 
+     * @param int $id
+     * @param int $paginate
+     * @return \App\Models\Post
+     */
+    protected static function getPostByUserId(int $id, int $paginate = 5)
+    {
+        return self::where('user_id', $id)->orderBy('created_at', 'desc')->paginate($paginate);
     }
 
+    /**
+     * Get commets when a post is requested
+     * 
+     * @return \App\Models\Comment
+     */
     public function comments()
     {
         return $this->hasMany('App\Models\Comment');
     }
-
+    
+    /**
+     * Get User (author) when a post is requested
+     * 
+     * @return \App\Models\User
+     */
     public function user()
     {
         return $this->hasOne('App\Models\User', 'id', 'user_id');
