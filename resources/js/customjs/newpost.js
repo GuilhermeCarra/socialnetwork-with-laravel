1c23208ef1995d8349305b3ef6230d91bba5f6af
Ezequiel <ezemgaray@gmail.com>
2020-09-27 23:38:05 +0200

    Update JS newpost.js - remove this.showNewPost()

--- resources/js/customjs/newpost.js
+++ resources/js/customjs/newpost.js
@@ -7,21 +7,13 @@
         cacheDom: function () {
             this.newPost = $('#newpost')
             this.form = $('#newpost-form')
-            this.button = $('#menu-newpost-btn')
         },
         bindEvents: function(){
-            this.button.on('click', this.showNewPost.bind(this))
             this.newPost.on('click', '.newpost-close', this.closeNewPost.bind(this))
             this.form.on('submit', this.send.bind(this))
         },
-        showNewPost: function(){
-            $('body').addClass('stop-scroll')
-            this.newPost.addClass('show')
-            // setTimeout(() => {this.input.focus()}, 500);
-        },
         send: function(e){
             e.preventDefault()
-
         },
         closeNewPost: function(e){
             this.form.trigger("reset")
