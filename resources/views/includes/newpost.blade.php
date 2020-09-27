0708abacdd23f1d77b0b0afd89d6652ca5a7786d
Ezequiel <ezemgaray@gmail.com>
2020-09-27 19:19:41 +0200

    Update View newpost.blad - change #newpost-form id

--- resources/views/includes/newpost.blade.php
+++ resources/views/includes/newpost.blade.php
@@ -1,9 +1,9 @@
 <div id="newpost" class="newpost d-flex flex-column justify-content-center align-items-center">
-    <div class="newpost-form" id="newpost-form">
+    <div class="newpost-form">
         <div class="newpost-close text-white">
             <i class="ri-close-circle-line text-white"></i>
         </div>
-        <form action="">
+        <form action="" id="newpost-form">
             <div class="form-group">
                 <label for="">Text</label>
                 <textarea class="form-control shadow" name="post_content" id="post_content" cols="30" rows="10"></textarea>
