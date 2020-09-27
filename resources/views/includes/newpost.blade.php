857fae56a4b52fcf493a1ef5155c4dcaf824d10a
Ezequiel <ezemgaray@gmail.com>
2020-09-27 19:00:06 +0200

    Add View newpost.blade - New Post form - in progress

---
+++ resources/views/includes/newpost.blade.php
@@ -0,0 +1,20 @@
+<div id="newpost" class="newpost d-flex flex-column justify-content-center align-items-center">
+    <div class="newpost-form" id="newpost-form">
+        <div class="newpost-close text-white">
+            <i class="ri-close-circle-line text-white"></i>
+        </div>
+        <form action="">
+            <div class="form-group">
+                <label for="">Text</label>
+                <textarea class="form-control shadow" name="post_content" id="post_content" cols="30" rows="10"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="post_image"></label>
+                <input type="file" name="post_image" id="post_image">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Post" class="btn btn-primary text-white shadow">
+            </div>
+        </form>
+    </div>
+</div>
