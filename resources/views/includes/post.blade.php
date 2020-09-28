c755e65b8c56c71ebda2eb23cbe361c1c951fa97
guilherme-carra <gui_gc@hotmail.com>
2020-09-28 13:27:31 +0200

    added class to work on javascript

--- resources/views/includes/post.blade.php
+++ resources/views/includes/post.blade.php
@@ -58,7 +58,7 @@
                         <a class="mr-2" href="{{ auth()->user()->username }}">
                             {{auth()->user()->name}}
                         </a>
-                        <p class="p-0 m-0"><small class="text-muted">Add a comment!</small></p>
+                        <p class="p-0 m-0"><small class="text-muted addComment-btn">Add a comment!</small></p>
                     </div>
                     <div class="comment_create">
                         <div class="form-group d-flex flex-nowrap justify-content-between">
@@ -71,8 +71,8 @@
         </div>
         {{-- @foreach ($post->comments as $comment) --}}
             @if(isset($post->comments[0]))
-                <div class="comment preview my-2">
-                    <div class="d-flex flex-start flex-nowrap align-items-center">
+                <div class="comment preview my-2 comments-container">
+                    <div class="d-flex flex-start flex-nowrap align-items-center comment">
                         <div class="comment__avatar__box">
                             <a class="" href="{{ $post->comments[0]->user->username }}">
                                 <img src="{{ $post->comments[0]->user->avatar  ?? asset('assets/img/ghost-line.svg')}}" class="rounded-circle avatar" alt="avatar">
@@ -91,7 +91,7 @@
                         </div>
                     </div>
                     @if ($post->comments_count > 1)
-                    <div class="mt-4 mb-2 p-0 btn text-center w-100 bg-light-gray pointer comments-btn" id="post_{{$post->id}}">
+                    <div class="mt-4 mb-2 p-0 btn text-center w-100 bg-light-gray pointer comments-btn comments-guide">
                         See more comments...
                     </div>
                     @endif
