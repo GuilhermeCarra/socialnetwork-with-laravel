bf2090897587050dd97b95a416ee9c4f47ba8ba2
Ezequiel <ezemgaray@gmail.com>
2020-09-28 02:35:43 +0200

    Update View navbar.blade - Reduce navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,19 +1,11 @@
-<div class="container">
+<div class="container flex justify-content-lg-between align-items-center">
     <a class="navbar-brand" href="{{ url('/') }}">
         {{ config('app.name') }}
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav mr-auto">
-
-        </ul>
 
         <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
+        <ul class="navbar-icons mb-0 ml-auto">
             <!-- Authentication Links -->
             @guest
             <li class="nav-item">
@@ -25,38 +17,28 @@
             </li>
             @endif
             @else
-            <li class="nav-item">
-                @include('includes.search_button')
+            
+            <li class="navbar-item" id="search-button">
+                {{-- @include('includes.search_button') --}}
+                <span class="navbar-link bg-blue-primary">
+                    <i class="ri-search-line text-white"></i>
+                </span>
             </li>
-            <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    <img class="avatar" src="{{ Auth::user()->avatar ?? asset('assets/img/ghost-line.svg') }}" alt="{{ Auth::user()->username }}'s avatar">
-                    {{ Auth::user()->username }}
+
+            <li class="navbar-item">
+                <a class="navbar-link" href="{{ route('home') }}"><i class="ri-home-5-{{Request::is('/') || Request::is('home') ? 'fill' : 'line'}}"></i></a>
+            </li>
+
+            <li class="navbar-item">
+                <a class="navbar-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+                    <i class="ri-logout-box-r-line"></i>
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    {{-- HOME --}}
-                    <a class="dropdown-item" href="{{ route('home') }}"><i class="ri-home-5-{{Request::is('/') || Request::is('home') ? 'fill' : 'line'}}"></i>Home</a>
-
-                    {{-- PROFILE --}}
-                    <a class="dropdown-item" href="{{ url(auth()->user()->username) }}"><i class="ri-user-3-{{ Request::is(auth()->user()->username) ? 'fill' : 'line'}}"></i>Profile</a>
-
-                    {{-- Edit PROFILE --}}
-                    <a class="dropdown-item" href="{{ url(auth()->user()->username.'/config') }}"><i class="ri-settings-4-{{Request::is('config') ? 'fill' : 'line'}}"></i>Edit Profile</a>
-
-                    {{-- LOGOUT --}}
-                    <hr>
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                        <i class="ri-logout-box-r-line"></i>{{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
             </li>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
             @endguest
         </ul>
-    </div>
 </div>
