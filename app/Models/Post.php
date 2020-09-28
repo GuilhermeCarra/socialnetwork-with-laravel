6b658d92f245e2e1877e0c13b9585f7f4a14903a
guilherme-carra <gui_gc@hotmail.com>
2020-09-28 13:24:43 +0200

    add func to list by creation date

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -33,7 +33,7 @@ class Post extends Model
      */
     public function comments()
     {
-        return $this->hasMany('App\Models\Comment');
+        return $this->hasMany('App\Models\Comment')->orderBy('created_at', 'desc');
     }
     
     /**
