a9e8dae0b75a0fa9bace12f55564f69898ecd3f1
guilherme-carra <gui_gc@hotmail.com>
2020-09-28 11:11:38 +0200

    added function to create a comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use App\Models\User;
+use Auth;
 
 class CommentsController extends Controller
 {
@@ -23,9 +24,20 @@ class CommentsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request, $id)
     {
-        //
+        if($request->ajax()) {
+
+            $user = auth()->user();
+            $comment = new Comment;
+            $comment->post_id = $id;
+            $comment->user_id = $user->id;
+            $comment->content = $request->content;
+            $comment->save();
+
+            $view = view('includes.comments',compact(['comment','user']))->render();
+            return $view;
+        }
     }
 
     /**
