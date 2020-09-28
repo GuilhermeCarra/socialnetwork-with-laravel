75bc3e005fde1a05a07dfa920459a1e0e6a8e374
Ezequiel <ezemgaray@gmail.com>
2020-09-28 02:53:22 +0200

    Update View menu.blade - Adding .shadow

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,8 +1,8 @@
 <div class="menu" id="menu">
     <ul>
-        <li class="menu-item text-center" id="menu-btn"><i class="ri-menu-2-fill"></i></li>
-        <li class="menu-item text-center" id="menu-newpost-btn"><i class="ri-menu-add-line"></i></li>
-        <li class="menu-item text-center" id="menu-friends-btn"><i class="ri-team-line"></i></li>
-        <li class="menu-item text-center"><a href="{{url(auth()->user()->username)}}"><i class="ri-user-3-line"></i></a></li>
+        <li class="menu-item text-center shadow" id="menu-btn"><i class="ri-menu-2-fill"></i></li>
+        <li class="menu-item text-center shadow" id="menu-newpost-btn"><i class="ri-menu-add-line"></i></li>
+        <li class="menu-item text-center shadow" id="menu-friends-btn"><i class="ri-team-line"></i></li>
+        <li class="menu-item text-center shadow"><a href="{{url(auth()->user()->username)}}"><i class="ri-user-3-line"></i></a></li>
     </ul>
 </div>
