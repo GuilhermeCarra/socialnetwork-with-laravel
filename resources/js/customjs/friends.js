2c5d2fb32e02639cbe13cb2b16ef67bced79fe23
Ezequiel <ezemgaray@gmail.com>
2020-09-29 10:20:43 +0200

    Update JS friends.js - Actions in friends list

--- resources/js/customjs/friends.js
+++ resources/js/customjs/friends.js
@@ -9,10 +9,14 @@
             this.form = $('#friends-form')
             this.input = $('#friends-input')
             this.results = $('#friends-result')
+            this.followingBtn = $('.following.btn')
+            this.followersBtn = $('.followers.btn')
         },
         bindEvents: function () {
             this.input.on('input', this.showResults.bind(this))
             this.form.on('click', '.friends-close', this.closeFriends.bind(this))
+            this.followersBtn.on('click', this.showFollowersList.bind(this))
+            this.followingBtn.on('click', this.showFollowingList.bind(this))
         },
         showResults: function () {
             this.results.addClass('searching').fadeIn()
@@ -26,6 +30,16 @@
                 $('#friends-result').html(data.html)
 
             })
+        },
+        showFollowersList: function(){
+            this.followersBtn.addClass('active')
+            this.followingBtn.removeClass('active')
+            $('.friends-list').addClass('followers')
+        },
+        showFollowingList: function(){
+            this.followersBtn.removeClass('active')
+            this.followingBtn.addClass('active')
+            $('.friends-list').removeClass('followers')
         },
         closeFriends: function () {
             this.input.val("")
