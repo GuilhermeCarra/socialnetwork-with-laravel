11c53a3f5b3828029fe8ea584e13eb3741192146
Ezequiel <ezemgaray@gmail.com>
2020-09-29 16:00:11 +0200

    Update Menu - adding navbar

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -3,6 +3,13 @@
         <li class="menu-item text-center shadow" id="menu-btn"><i class="ri-menu-2-fill"></i></li>
         <li class="menu-item text-center shadow" id="menu-newpost-btn"><i class="ri-menu-add-line"></i></li>
         <li class="menu-item text-center shadow" id="menu-friends-btn"><i class="ri-team-line"></i></li>
-        <li class="menu-item text-center shadow"><a href="{{url(auth()->user()->username)}}"><i class="ri-user-3-line"></i></a></li>
+        <li class="menu-item text-center shadow"><a class="navbar-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
+                <i class="ri-logout-box-r-line"></i>
+            </a>
+        </li>
     </ul>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
 </div>
