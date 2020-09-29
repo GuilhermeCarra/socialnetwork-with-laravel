f6ed06cbfa5d1c1b084486db90aeee67c892f786
Ezequiel <ezemgaray@gmail.com>
2020-09-29 16:01:38 +0200

    Update web.php - adding route to update post

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware('auth')->group(function(){
     Route::get('/search', [App\Http\Controllers\FollowsController::class, 'showFriends']);
     Route::get('/{username}', [App\Http\Controllers\UsersController::class, 'index']);
+    Route::post('/post/show', [App\Http\Controllers\PostsController::class, 'show']);
     Route::get('/comments/post/{id}', [App\Http\Controllers\CommentsController::class, 'loadPostComments']);
     Route::post('/comments/create/{id}', [App\Http\Controllers\CommentsController::class, 'create']);
     Route::delete('/comments/delete/{id}', [App\Http\Controllers\CommentsController::class, 'destroy']);
